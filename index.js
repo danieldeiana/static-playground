fee551791a8272d62740c80811e17cc6f6619e3a
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-29 19:42:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Dynamically render the document and header - h1 title
+const newTitle = 'Static Playground';
+const headerTitle = document.getElementById('header-title');
+
+document.title = newTitle;
+headerTitle.innerText = newTitle;
+
+// Dynamically render copywright text
+const currentYear = new Date().getFullYear();
+var copywrightP = document.getElementById('copywright');
+const copywrightText = '@copy' + currentYear + ' Daniel Deiana';
+copywrightP.innerText = copywrightText;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Static Playground
+
+## About
+This is a template for practicing static fundamentals
+
+It contains a pre-linked HTML, CSS and Javascript document
+
+## Clone it 
+
+Create your directory, `cd` in, and run the following from the terminal:
+
