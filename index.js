07f2f5c7e73c7bd7aa6b146fe93b2f721caee8f6
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-31 17:20:51 +0100

    updated css and js files

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-// Delete once implemented
-alert(
-`Change your title var named newTitle within index.js
-then delete this alert`
-)
+// ******************
+// **** USER SET ****
+// ****************** 
+const newTitle = "SET TITLE IN INDEX.JS (line 4)";
+const author = "SET AUTHORS NAME IN INDEX.JS (line 5)";
 
-// Dynamically render the document and header - h1 title
-const newTitle = 'Static Playground';
+// *************************
+// **** CONFIG SETTINGS ****
+// *************************
+
+// -Set- scroll to top of page on load / refresh
+var scrollOnRefresh = false;
+
+// *************************
+// **** DYNAMIC CONTENT ****
+// *************************
+
+// Get and set the header title
 const headerTitle = document.getElementById('header-title');
-
-document.title = newTitle;
 headerTitle.innerText = newTitle;
+
+// Set the document title (displayed in tab)
+document.title = newTitle;
 
 // Dynamically render copywright text
 const currentYear = new Date().getFullYear();
 var copywrightP = document.getElementById('copywright');
-const copywrightText = '@copy' + currentYear + ' Daniel Deiana';
+const copywrightText = '@copy' + currentYear + ' ' + author;
 copywrightP.innerText = copywrightText;
+
+// *********************
+// **** CONFIG CODE ****
+// *********************
+
+// -Define- scroll to top of page on load / refresh
+window.onbeforeunload = function () {
+    if(scrollOnRefresh){
+        window.scrollTo(0, 0);
+    };
+  };
+
+// *************************************************************
